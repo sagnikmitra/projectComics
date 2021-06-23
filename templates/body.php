b4918891646dd934d4a1e4054f78ab8ce811727e
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2021-06-24 01:10:32 +0530

    Update body.php

--- templates/body.php
+++ templates/body.php
@@ -3,7 +3,7 @@
     <div class="container">
 
         <h3>Enter Your Email to Unsubscribe</h3>
-        <form action="http://localhost:63342/email/templates/unsubscribed.php" method="post">
+        <form action="https://projectcomics.herokuapp.com/templates/unsubscribed.php" method="post">
             <label for="email">Email: </label>
             <input id="email" type="text" name="email" placeholder="yourmail@email.com">
             <input class="btn btn-danger" type="submit" name="submit" value="Unsubscribe">
