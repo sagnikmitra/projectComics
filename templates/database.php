9487e9d5cace81066577b199690d359ed7b25691
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2021-06-25 19:49:30 +0530

    Update database.php

--- templates/database.php
+++ templates/database.php
@@ -3,5 +3,11 @@ $connection = mysqli_connect("remotemysql.com",
     "LydSLSny7j",
     "hOWxQIyzud",
     "LydSLSny7j");
+if($connection){
+    echo "connected";
+    }
+else{
+    echo "not connected";
+}
 
 ?>
