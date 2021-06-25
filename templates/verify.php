ca8f589966f839796287f4da0f699310f6f86e85
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2021-06-25 20:18:24 +0530

    Update verify.php

--- templates/verify.php
+++ templates/verify.php
@@ -1,8 +1,6 @@
 <?php include "database.php"  ?>
 <?php
 $username = $_GET["username"];
-echo $username;
-echo "<br>";
 $connection = mysqli_connect("remotemysql.com",
     "LydSLSny7j",
     "hOWxQIyzud",
@@ -14,23 +12,15 @@ $encryption_iv = '5489894647979744';
 $encryption_key = "therewillbevkcpridemcbcfktu";
 $encript_username = openssl_encrypt($username, $ciphering,
     $encryption_key, $options, $encryption_iv);
-echo $encript_username;
-
 $otp = $_GET['otp'];
- echo "<br>";
-    echo $otp;
 $querys = "SELECT * FROM users";
 $result = mysqli_query($connection,$querys);
 while ($row = mysqli_fetch_assoc($result)){
 
     $data_otp = $row['otp'];
-    echo "<br>";
-    echo $data_otp;
     if ($otp == $data_otp)
     {
         $query = "UPDATE users SET sub='yes' WHERE username = '$encript_username'";
-        echo "<br>";
-    echo $query;
         $updates = mysqli_query($connection,$query);
         if(isset($updates))
         {?>
