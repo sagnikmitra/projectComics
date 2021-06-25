fe1ccd6d83e1c5baf474fcb8f565f458080a99a3
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2021-06-26 04:30:32 +0530

    Update config.php

--- config.php
+++ config.php
@@ -46,8 +46,7 @@ require "SMTP.php";
         $mail->addEmbeddedImage("myImage.jpg", "image.jpg", "image.jpg", "base64", "image/jpeg");
         $mail->isHTML(true);
         $mail->Subject = "COMICS";
-        $mail->Body = "<h3>$sub</h3><em>$body</em><br><img src='cid:image.jpg' alt='image'/><br><h3><a href='https://projectcomics.herokuapp.com/templates/unsubscribed.php?username=$decryptions'>Unsubscribe</a></h3>";
-
+        $mail->Body = "<h3>$sub</h3><em>$body</em><br><br><img src='cid:image.jpg' alt='image'/><br><h3><a href='https://projectcomics.herokuapp.com/templates/unsubscribed.php?username=$decryptions'>Unsubscribe</a></h3>";
         $mail->send();
 
     }
