606b0dae000fc736526161de333bee842690b843
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2021-06-25 20:42:29 +0530

    Update unsubscribed.php

--- templates/unsubscribed.php
+++ templates/unsubscribed.php
@@ -1,16 +1,26 @@
 <?php include "database.php" ?>
 <?php
 ini_set('display_errors', 0);
-$email = $_POST["email"];
+$email = $_GET["username"];
 $query = "SELECT * FROM users";
 $result = mysqli_query($connection,$query);
+$ciphering = "AES-256-CBC";
+$options = 0;
+$encryption_iv = '5489894647979744';
+$encryption_key = "therewillbevkcpridemcbcfktu";
+$decryption_iv = '5489894647979744';
+$decryption_key = "therewillbevkcpridemcbcfktu";
+
 while($row = mysqli_fetch_assoc($result))
 {
 
     $database_username = $row["username"];
-    if ($email==$database_username)
+    $decrypt = openssl_decrypt($database_username, $ciphering, $decryption_key, $options, $decryption_iv);
+    $encript = openssl_encrypt($email, $ciphering,
+    $encryption_key, $options, $encryption_iv);
+    if ($email==$decript)
     {
-        $query = "DELETE FROM users WHERE username = '$email'";
+        $query = "DELETE FROM users WHERE username = '$encript'";
         $result = mysqli_query($connection,$query);
         if(!$result)
         {
