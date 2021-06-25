0168447633e1cb7223274c1faca8a44cb86e1a9b
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2021-06-25 22:05:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 <body>
     <h2>Welcome</h1>
     <br>
+    <ul>
     <p>Enter you email address to get Comies every five minutes</p>
+        <li>First you need to register</li>
+    </ul>
 <form action="https://projectcomics.herokuapp.com/login.php" method="GET" >
     <label for="user">Enter Your Email:</label>
     <input type="email" name="username" placeholder="Username">
