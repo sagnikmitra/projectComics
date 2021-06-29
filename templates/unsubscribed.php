9f5ceec48fade43a7441646b094eb91dc47ef073
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2021-06-29 22:20:17 +0530

    Update unsubscribed.php

--- templates/unsubscribed.php
+++ templates/unsubscribed.php
@@ -1,7 +1,8 @@
 <?php include "database.php" ?>
 <?php
 ini_set('display_errors', 0);
-$email = $_GET["username"];
+$token = $_GET["vf1"];
+$otp = $_GET['vf2'];
 $query = "SELECT * FROM users";
 $result = mysqli_query($connection,$query);
 $ciphering = "AES-256-CBC";
@@ -13,13 +14,13 @@ $decryption_key = "therewillbevkcpridemcbcfktu";
 
 while($row = mysqli_fetch_assoc($result))
 {
-
+    $database_token = $row["token"];
+    $database_otp = $row["otp"];
     $database_username = $row["username"];
-    $decrypt = openssl_decrypt($database_username, $ciphering, $decryption_key, $options, $decryption_iv);
-    $encript = openssl_encrypt($email, $ciphering,$encryption_key, $options, $encryption_iv);
-    if ($email==$decrypt)
+    echo $database_username;
+    if ($database_otp==$otp && $database_token==$token)
     {
-        $query = "DELETE FROM users WHERE username = '$encript'";
+        $query = "DELETE FROM users WHERE username = '$database_username'";
         $result = mysqli_query($connection,$query);
         if(!$result)
         {
@@ -38,7 +39,7 @@ while($row = mysqli_fetch_assoc($result))
                  }
 
 }
-if ($email !=$database_username)
+if ($database_token !=$token || $database_otp != $otp)
 {?>
 
 
