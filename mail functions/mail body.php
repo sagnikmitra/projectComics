6ae56fe27f80e8b6507f799eaf6e2045d4a1a724
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2021-06-29 22:20:40 +0530

    Update mail body.php

--- mail functions/mail body.php
+++ mail functions/mail body.php
@@ -4,7 +4,7 @@ $mail->Host = "smtp.gmail.com";
 $mail->Port = 587;
 $mail->SMTPAuth = true;
 $mail->SMTPSecure = 'tls';
-$mail->Username = "";#your email
-$mail->Password = "";#your password
+$mail->Username = "shuvratcp@gmail.com";
+$mail->Password = "iamacool";
 $mail->setFrom("shuvratcp@gmail.com");
 ?>
