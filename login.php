67ea5dd9b20be539d776de1005c1e235c76d0030
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2021-06-30 00:10:55 +0530

    Update login.php

--- login.php
+++ login.php
@@ -14,34 +14,44 @@ $encript_username = openssl_encrypt($username, $ciphering,
     $encryption_key, $options, $encryption_iv);
 $token = md5(uniqid($username, true));
 $otp = rand(152698,986452);
-$query = "INSERT INTO users(username,token,sub,otp)";
-$query .= "VALUES ('$encript_username','$token','no','$otp')";
-$results = mysqli_query($connection,$query);
-$mail = new PHPMailer;
-?>
-<?php include 'mail functions/mail body.php' ?>
-<?php
-$mail->addAddress("$username");
-$mail->addReplyTo("shuvratcp@gmail.com");
-$mail->isHTML(true);
-$mail->Subject = "COMICS";
-$mail->Body = "<h3>Verification Mail</h3><p><em>This is the verification mail to activate you account! Hurry! Click on verification </em></p></nr><h3><a href='https://projectcomics.herokuapp.com/templates/verify.php?username=$username&token=$token'>Verification</a></h3>";
+$if_exist = "SELECT * FROM users WHERE username ='$encript_username'";
+$result = mysqli_query($connection,$if_exist);
+$row = mysqli_num_rows($result);
+if( $row === 0 )
+{
+    $query = "INSERT INTO users(username,token,sub,otp)";
+    $query .= "VALUES ('$encript_username','$token','no','$otp')";
+    $results = mysqli_query($connection,$query);
+    $mail = new PHPMailer;
+    ?>
+    <?php include 'mail functions/mail body.php' ?>
+    <?php
+    $mail->addAddress("$username");
+    $mail->addReplyTo("shuvratcp@gmail.com");
+    $mail->isHTML(true);
+    $mail->Subject = "COMICS";
+    $mail->Body = "<h3>Verification Mail</h3><p><em>This is the verification mail to activate you account! Hurry! Click on verification </em></p></nr><h3><a href='https://projectcomics.herokuapp.com/templates/verify.php?username=$username&token=$token'>Verification</a></h3>";
 
-if (!$mail->send()) { ?>
-    <?php include "templates/header.php" ?>
-    <body>
-           <h3>Something went wrong!</h3>
-    </body>
+    if (!$mail->send()) { ?>
+        <?php include "templates/header.php" ?>
+        <body>
+               <h3>Something went wrong!</h3>
+        </body>
 
 
-<?php }  else { ?>
+    <?php }  else { ?>
 
-    <?php include "templates/header.php" ?>
-    <body class="mw">
-          <h2 style="color: #2EC486">Verification Link Send to your Email</h2>
-    </body>
+        <?php include "templates/header.php" ?>
+        <body class="mw">
+              <h2 style="color: #2EC486">Verification Link Send to your Email</h2>
+        </body>
 
 
 <?php }
+}
+else
+{
+    echo "<h2 style='color: #2EC486'>User alredy exists</h2> <br> <p style='color: darkblue'>If you want to Unsubscribe Please check your Email</p>";
+}
 
 ?>
