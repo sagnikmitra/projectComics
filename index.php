e1f938186cb34464f5d842d6900a6be18a7d9bde
Sagnik Mitra <sagnikmitra123@gmail.com>
2021-09-21 16:50:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include "templates/header.php" ?>
 <body class="mw"  >
 <h1 style="color: white">Welcome</h1>
-    <p style="color: white">Enter you email address to get Comies every five minutes</p>
+    <p style="color: white">Enter you email address to get Comics every five minutes</p>
     <form  action="https://projectcomics.herokuapp.com/login.php" method="GET" >
         <label id="lable" for="user">Enter Your Email:</label>
         <input id="user" type="email" name="username" placeholder="yourmail@mail.com">
